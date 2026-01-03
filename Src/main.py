796ea5b6a3161151b038537c799b6f1139869abf
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2026-01-03 19:21:50 +0530

    update preprocessing main

--- Src/main.py
+++ Src/main.py
@@ -23,16 +23,6 @@ def main():
     test_pre.run_preprocessing()
     print("Test data preprocessing complete")
 
-    splitter = Split(
-        input_path="Data/Processed/processed_train.csv",
-        output_dir="Data/Split/",
-        test_size=0.2,
-        random_state=42
-    )
 
-    splitter.run()
-    print("Train-test split completed successfully")
-
-    
 if __name__ == "__main__":
     main()
